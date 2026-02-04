ef9b08bca8ba48e46f5c7e0363166abe729d14cf
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-02-04 23:23:02 +0800

    feat: Add navbar component and replace header in home view

---
+++ landing-page/resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="py-12">
+    <div class="mx-auto max-w-6xl">
+        <nav class="flex items-center justify-between">
+            <h1 class="font-bold text-3xl">Logo</h1>
+
+            <div class="space-x-8">
+                <a href="#">Features</a>
+                <a href="#">Docs</a>
+                <a href="#">Company</a>
+            </div>
+
+            <div>
+                <a href="#">Log In</a>
+            </div>
+        </nav>
+    </div>
+</header>
--- landing-page/resources/views/home.blade.php
+++ landing-page/resources/views/home.blade.php
@@ -4,23 +4,8 @@
 <x-head />
 
 <body class="bg-gray-200">
-    <header class="py-12">
-        <div class="mx-auto max-w-6xl">
-            <nav class="flex items-center justify-between">
-                <h1 class="font-bold text-3xl">Logo</h1>
 
-                <div class="space-x-8">
-                    <a href="#">Features</a>
-                    <a href="#">Docs</a>
-                    <a href="#">Company</a>
-                </div>
-
-                <div>
-                    <a href="#">Log In</a>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <x-navbar />
 
     <section class="py-8">
         <div class="mx-auto max-w-6xl gap-y-32">
