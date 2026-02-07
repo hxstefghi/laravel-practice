e017dd26df3aaa4cddf6ab0066a2016bd5a88df5
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-02-07 21:55:31 +0800

    feat: Implement user registration functionality with validation and views

--- landing-page/resources/views/register.blade.php
+++ landing-page/resources/views/register.blade.php
@@ -8,28 +8,32 @@
     <section>
         <div class="py-12 space-y-6">
             <div class="text-center mb-6">
-                <h1 class="font-bold text-3xl">Login to your account</h1>
+                <h1 class="font-bold text-3xl">Register an account</h1>
             </div>
 
             <div class="mx-auto max-w-md">
-                <form action="{{ route('login') }}" method="POST">
+                <form action="{{ route('register.store') }}" method="POST">
                     @csrf
 
                     <div class="flex flex-col space-y-2">
+                        <x-input-field type="text" label="Full Name" name="name" />
+
                         <x-input-field type="email" label="Email" name="email" />
 
                         <x-input-field type="password" label="Password" name="password" />
 
+                        <x-input-field type="password" label="Confirm Password" name="password_confirmation" />
+
                         <button
-                            class="py-2 px-3 rounded-lg bg-gray-900 hover:bg-gray-800 text-white mt-2 cursor-pointer">Login</button>
+                            class="py-2 px-3 rounded-lg bg-gray-900 hover:bg-gray-800 text-white mt-2 cursor-pointer">Register</button>
                     </div>
                 </form>
             </div>
 
             <div class="text-center">
-                <p>Don't have an account?
-                    <a href="{{ route('register') }}">
-                        <span class="underline">Register</span>
+                <p>Already have an account?
+                    <a href="{{ route('login-view') }}">
+                        <span class="underline">Login</span>
                     </a>
                 </p>
             </div>
--- landing-page/routes/web.php
+++ landing-page/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::view('/login', 'login')->name('login-view');
+
+// Route::view('/register', 'register')->name('register-view');
+Route::resource('/register', UserController::class);
+Route::get('/register', [UserController::class, 'create'])->name('register');
