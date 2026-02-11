e482c3386b0725ed21d27ad2cbc715cbff3fff96
hxstefghi <cccatuday1401qc@student.fatima.edu.ph>
2026-02-11 22:33:51 +0800

    feat: Enhance post display layout with structured sections and author information

--- landing-page/resources/views/home.blade.php
+++ landing-page/resources/views/home.blade.php
@@ -8,9 +8,26 @@
     <x-navbar />
     @auth
         @foreach ($posts as $post)
-            <h1>{{ $post->user->name }}</h1>
-            <h1>{{ $post->title }}</h1>
-            <p>{{ $post->body }}</p>
+            <section class="py-6">
+                <div class="mx-auto max-w-xl">
+                    <div class="flex flex-col">
+                        <div class="bg-white p-6 rounded-2xl">
+                            <div class="flex justify-between">
+                                <h1>{{ $post->user->name }}</h1>
+                                <h1>{{ $post->user->email }}</h1>
+                            </div>
+                            <hr class="my-4">
+                            <div class="flex flex-between flex-col">
+                                <div class="mt-2 mb-6">
+                                    <h1>{{ $post->title }}</h1>
+                                    <p>{{ $post->body }}</p>
+                                </div>
+                                <p class="text-gray-500 text-sm">Posted {{ $post->updated_at->diffForHumans() }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         @endforeach
     @else
         <x-guest-home />
